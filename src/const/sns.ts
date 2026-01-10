c0b5b221df493be2baa377ac1358073e9bb79e73
Yo Iwamoto <you.iwamoto918@gmail.com>
2026-01-10 18:36:29 +0900

    fix

--- src/const/sns.ts
+++ src/const/sns.ts
@@ -1,28 +1,13 @@
 export const sns = [
   {
-    name: 'mixi2',
-    href: 'https://mixi.social/@yoiwamoto',
-    imagePath: '/assets/mixi2.svg',
-  },
-  {
     name: 'X',
     href: 'https://x.com/yoiwamoto',
     imagePath: '/assets/x.png',
   },
   {
-    name: 'Bluesky',
-    href: 'https://yoiwamoto.bsky.social',
-    imagePath: '/assets/bluesky.png',
-  },
-  {
     name: 'GitHub',
     href: 'https://github.com/yo-iwamoto',
     imagePath: '/assets/github.svg',
-  },
-  {
-    name: 'Threads',
-    href: 'https://www.threads.net/@yoiwamoto_',
-    imagePath: '/assets/threads.svg',
   },
   {
     name: 'Zenn',
